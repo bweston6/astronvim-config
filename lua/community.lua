3b384bbb2039891b824501f1c7d43ef2f3fb26b6
Ben Weston <35574350+bweston6@users.noreply.github.com>
2024-10-03 12:10:36 +0100

    Initial commit

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,26 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  --[[ Language Packs ]]
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.markdown" },
+  {
+    -- requires composer
+    import = "astrocommunity.pack.php"
+  },
+  {
+    -- requires python
+    import = "astrocommunity.pack.python"
+  },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+
+  --[[ Filetype Plugins ]]
+  { import = "astrocommunity.programming-language-support.csv-vim" },
 }
