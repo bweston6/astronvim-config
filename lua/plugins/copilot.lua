50b74974ce23043d0fe83b8472268bc00d84f0d1
Ben Weston <b.weston60@gmail.com>
2025-06-09 09:39:24 +0100

    Add copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+---@type LazySpec
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function() require("copilot").setup {} end,
+    },
+    {
+        "olimorris/codecompanion.nvim",
+        opts = {},
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+    },
+}
