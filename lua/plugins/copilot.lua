6a4237c17c75f756d8537772966af36e744d10d2
Ben Weston <b.weston60@gmail.com>
2025-06-27 15:44:58 +0100

    Set default model and add config for LMStudio

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,17 +1,48 @@
 ---@type LazySpec
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        config = function() require("copilot").setup {} end,
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function() require("copilot").setup {} end,
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
-    {
-        "olimorris/codecompanion.nvim",
-        opts = {},
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("codecompanion").setup {
+        opts = {
+          show_defaults = false,
         },
-    },
+        adapters = {
+          copilot = function()
+            return require("codecompanion.adapters").extend("copilot", {
+              schema = {
+                model = {
+                  default = "gemini-2.5-pro-preview-06-05",
+                },
+              },
+            })
+          end,
+          my_openai = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              env = {
+                url = "http://windows.lan:1234",
+              },
+              schema = {
+                model = {
+                  default = "google/gemma-3-12b",
+                  -- default = "qwen/qwen-3-14b",
+                },
+              },
+            })
+          end,
+        },
+      }
+    end,
+  },
 }
