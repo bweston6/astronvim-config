56d2651655650c3467ae3189c7915d444fad6791
Ben Weston <b.weston60@gmail.com>
2026-01-21 13:31:54 +0000

    Change colour scheme

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrotheme", "habamax" } },
+  install = { colorscheme = { "astrotheme", "alabaster" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
